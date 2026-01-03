f38657ca610e27ba61d1d5f24130811e45248266
vansideee <strakovichvanya@gmail.com>
2026-01-03 20:56:26 +0300

    Новинка: Добавлено хранилище в оперативной памяти и конечная точка GET /tasks

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request # добавили request
 
 app = Flask(__name__)
 
-# Главная страница, чтобы проверить, что сервер жив
+# Имитация БД в памяти
+tasks = [
+    {
+        "id": 1,
+        "title": "Купить молоко",
+        "description": "Обязательно 3.2% жирности",
+        "status": "todo"
+    },
+    {
+        "id": 2,
+        "title": "Запустить API",
+        "description": "Сделать первый запрос",
+        "status": "in_progress"
+    }
+]
+
 @app.route('/')
 def home():
     return "To-Do List API is running!"
+
+# 1. Получить список всех задач
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
